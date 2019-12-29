71a39f3fd363c4fcd50598212ae71ce26517d570
Run Jiao <jiaorunnju@gmail.com>
2019-12-29 19:56:34 +0800

    cifar10 task added

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,1 +1,2 @@
 from .image_classify_task import ImageClassifyTask
+from .cifar10_task import Cifar10Task
---
+++ tasks/cifar10_task.py
@@ -0,0 +1,53 @@
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from .train_task import TrainTask
+
+
+class Cifar10Task(TrainTask):
+
+    def get_model(self):
+        return models.resnet18()
+
+    def get_train_dataset(self, path):
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        return datasets.CIFAR10(path, train=True, transform=transform)
+
+    def get_valid_dataset(self, path):
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        return datasets.CIFAR10(path, train=False, transform=transform)
+
+    def get_test_dataset(self, path):
+        pass
+
+    def get_criterion(self):
+        return torch.nn.CrossEntropyLoss()
+
+    def get_metric(self):
+        def accuracy(output, target, topk=(1,)):
+            """Computes the accuracy over the k top predictions for the specified values of k"""
+            with torch.no_grad():
+                maxk = max(topk)
+                batch_size = target.size(0)
+
+                _, pred = output.topk(maxk, 1, True, True)
+                pred = pred.t()
+                correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+                res = []
+                for k in topk:
+                    correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+                    res.append(correct_k.mul_(100.0 / batch_size))
+                return res
+
+        return {
+            "name": ['Acc@1', ],
+            "metric": lambda x, y: accuracy(x, y, topk=(1,)),
+
+        }
+
+    def update_metric(self, old, new):
+        return new > old, max(old, new)
